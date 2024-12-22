fae5709819e5da5733f954c41e9f3978c2e8b8eb
Panther010 <48002647+Panther010@users.noreply.github.com>
2024-12-22 16:00:19 +0000

    Pyspark q6 update

---
+++ Pyspark/Practice_beginner/jobs/06_most_popular_&_obscure_superhero.py
@@ -0,0 +1,38 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as f
+from pyspark.sql.types import *
+
+spark = SparkSession.builder.appName('popular hero').master('local').getOrCreate()
+
+hero_graphs = '../input_data/Marvel-graph.txt'
+hero_names = '../input_data/Marvel-names.txt'
+
+graphs = spark.read.text(hero_graphs)
+
+name_schema = StructType([StructField('hero_id', IntegerType(), True),
+                          StructField('hero_name', StringType(), True)])
+
+names = spark.read.csv(hero_names, sep=' ', header=False, inferSchema=True, schema=name_schema)
+
+graphs.printSchema()
+graphs.show()
+
+heroes = graphs.select(f.split(f.col('value'), ' ')[0].alias('hero_id'),
+                       (f.size(f.split(f.col('value'), ' ')) - 1 ).alias('connection')) \
+          .groupby(f.col('hero_id')).agg(f.sum(f.col('connection')).alias('total_connections'))
+
+
+heroes.printSchema()
+heroes.show()
+
+names.printSchema()
+names.show()
+
+result = heroes.alias('heroes').join(names.alias('names'), f.col('heroes.hero_id') == f.col('names.hero_id')) \
+        .select(f.col('heroes.hero_id'), f.col('names.hero_name'), f.col('total_connections')) \
+        .orderBy(f.col('total_connections').desc())
+
+result.printSchema()
+result.show()
+
+query = """with """
