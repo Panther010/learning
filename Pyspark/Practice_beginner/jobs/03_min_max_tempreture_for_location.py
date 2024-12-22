0797100877440b0d8d19883226a6b5671fced6dc
Panther010 <48002647+Panther010@users.noreply.github.com>
2024-12-22 08:49:09 +0000

    data for pyspark code

--- Pyspark/Practice_beginner/jobs/03_min_max_tempreture_for_location.py
+++ Pyspark/Practice_beginner/jobs/03_min_max_tempreture_for_location.py
@@ -12,18 +12,43 @@ columns = ['loc_id', 'date_time', 'data_type', 'temp_f', 'col_5', 'flag', 'col_7
 temperature_data = temperature_data.toDF(*columns)
 
 temperature_data.printSchema()
-temperature_data.show()
 
 temperature_data.createOrReplaceTempView('temperature')
 spark.sql("select * from temperature").show()
 
-query = """select loc_id, 
-                min(temp_f) as min_temp_f
+min_query = """select loc_id, 
+                min(temp_f) as min_temp_f,
+                (min(temp_f) -32) * (5/9) as min_temp_c
             from temperature 
             where data_type = 'TMIN'
             group by loc_id"""
 
-results = spark.sql(query)
+min_results = spark.sql(min_query)
 
-results.printSchema()
-results.show()
+max_query = """select loc_id,
+                    max(temp_f) as max_temp_f,
+                    (max(temp_f) -32) * (5/9) as max_temp_c
+            from temperature 
+            where data_type = 'TMAX'
+            group by loc_id
+                """
+
+max_results = spark.sql(max_query)
+
+df_min = temperature_data \
+            .filter(f.col('data_type') == f.lit('TMIN')) \
+            .groupby(f.col('loc_id')) \
+            .agg(f.min(f.col('temp_f')).alias('min_temp_f'),
+                 ((f.min(f.col('temp_f')) - 32) * (5/9)).alias('min_temp_c'))
+
+df_min.printSchema()
+df_min.show()
+
+df_max = temperature_data \
+            .filter(f.col('data_type') == f.lit('TMAX')) \
+            .groupby(f.col('loc_id')) \
+            .agg(f.max(f.col('temp_f')).alias('max_temp_f'),
+                 ((f.max(f.col('temp_f')) - 32) * (5/9)).alias('max_temp_c'))
+
+df_max.printSchema()
+df_max.show()
---
+++ Pyspark/Practice_beginner/jobs/04_total_amount_by_cust.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as f
+from pyspark.sql.types import *
+
+spark = SparkSession.builder.appName("total order by customer").master("local").getOrCreate()
+
+input_file = '../input_data/customer-orders.csv'
+
+schema = StructType([StructField('cust_id', IntegerType(), True),
+                      StructField('order_id', IntegerType(), True),
+                      StructField('amount', FloatType(), True)])
+
+orders = spark.read.csv(input_file, header=False, schema=schema)
+orders.printSchema()
+orders.createOrReplaceTempView('orders')
+orders.show()
+
+cust_orders = orders.groupby(f.col('cust_id')).agg(f.sum(f.col('amount')).alias('total_amount')).orderBy(f.col('total_amount').desc())
+
+cust_orders.printSchema()
+cust_orders.show()
+
+query = """select cust_id, sum(amount) as total_amount from orders group by cust_id order by sum(amount) desc"""
+cust_ord_s = spark.sql(query)
+cust_ord_s.show()
