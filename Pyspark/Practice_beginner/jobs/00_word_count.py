ea7d5947712d169eb097b7002d39381b3ac82dbb
Panther010 <48002647+Panther010@users.noreply.github.com>
2024-12-31 00:50:02 +0000

    SQL 20 Solved

--- Pyspark/Practice_beginner/jobs/00_word_count.py
+++ Pyspark/Practice_beginner/jobs/00_word_count.py
@@ -6,48 +6,81 @@ and outputs the results sorted by frequency using both DataFrame API and SQL.
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
 
-# 1. Initialize Spark Session
-spark = SparkSession.builder.appName("00 word count").master("local").getOrCreate()
 
-# file paths
-input_path = "../input_data/words.txt"
-output_path = "../output/words_count"
+class WordsCount:
 
-# 2. Define File Paths for Input and Output
-stop_words = ["is", "the", "a", "an", "and", "to", "in", "of", "on", "for", "with", "at", " ", "he", "she", "his", "was", "had", "that", "it", "as", ""]
-stop_words_str = ",".join([f"'{word}'" for word in stop_words])
-print(stop_words_str)
+    def __init__(self):
+        # 1. Initialize Spark Session
+        self.spark = SparkSession.builder.appName('wc').master('local').getOrCreate()
 
-# 4. Read Data from Input File
-df = spark.read.text(input_path)
-df.createOrReplaceTempView("book_data")
+        # file paths
+        self.input_path = "../input_data/words.txt"
+        self.output_path = "../output/words_count"
 
-# 5. Split Words and Filter Stop Words using DataFrame API
-df_words = df.select(
-    f.explode(  # convert all the words into separate row
-        f.split(  # split the words on spaces
-            f.lower(f.col("value")),
-            " ")).alias("words")) \
-    .filter(~f.col("words").isin(stop_words))
+        # 2. Define File Paths for Input and Output
+        self.stop_words = ["is", "the", "a", "an", "and", "to", "in", "of", "on", "for", "with", "at", " ", "he", "she",
+                      "his", "was", "had", "that", "it", "as", ""]
+        self.stop_words_str = ",".join([f"'{word}'" for word in self.stop_words])
+        print(self.stop_words_str)
 
-# 6. Count Words and Order by Frequency
-df_words_count = df_words.groupby(f.col("words"))  \
-                  .agg(f.count("words").alias("words_count"))  \
-                  .orderBy(f.col("words_count").desc())
+    # Read Data from Input File
+    def read_data(self):
+        return self.spark.read.text(self.input_path)
 
-# 7. Alternatively, Using SQL for Word Count
-query = f""" with words_tb as (
-                select explode(split(lower(value), " ")) as words from book_data)
+    # Split Words and Filter Stop Words using DataFrame API
+    def split_words(self, line_df):
+        return line_df.select(
+            f.explode(
+                f.split(
+                    f.lower(f.col('value'))
+                , ' ')).alias('words')
+            )
+
+    # Clean the words by removing Null, unwanted characters and filter unwanted words
+    def words_cleaner(self, words_df):
+        return words_df.withColumn('words', f.regexp_replace(f.col('words'), r'[^a-z]', '')) \
+            .filter(~f.col('words').isin(self.stop_words) & f.col('words').isNotNull())
+
+    # Count Words and Order by Frequency
+    def words_counter(self, words_df):
+        return words_df.groupby('words').agg(f.count('words').alias('words_count'))
+
+    # write the data to output directory
+    def writer(self, word_count_df):
+        word_count_df.printSchema()
+        word_count_df.orderBy(f.col('words_count').desc()).show()
+        print(word_count_df.count())
+        # word_count_df.write.parquet(self.output_path)
+
+    def sql_wc(self, lines_df):
+        lines_df.createOrReplaceTempView("lines")
+        return self.spark.sql(f"""
+        with words_tb as (
+                select explode(split(lower(value), " ")) as words from lines),
+            clean_words_tb as (
+                select regexp_replace(words, '[^a-z]', '') as words from words_tb)
             select 
                 words, 
                 count(1) as words_count 
-            from words_tb
-            where words not in ({stop_words_str}) 
+            from clean_words_tb
+            where words not in ({self.stop_words_str}) 
             group by words 
-            order by count(1) desc"""
+            order by count(1) desc
+        """)
 
-result = spark.sql(query)
+    # diver program
+    def execute(self):
+        lines = self.read_data()
+        words = self.split_words(lines)
+        clean_words = self.words_cleaner(words)
+        words_count = self.words_counter(clean_words)
+        self.writer(words_count)
 
-result.coalesce(1).write.csv(output_path, mode="overwrite", header=True)
-# result.show()
-spark.stop()
+        sql_wc_data = self.sql_wc(lines)
+        sql_wc_data.show()
+        self.spark.stop()
+
+
+if __name__ == '__main__':
+    wc = WordsCount()
+    wc.execute()
--- Pyspark/Practice_beginner/jobs/01_rating_counter.py
+++ Pyspark/Practice_beginner/jobs/01_rating_counter.py
@@ -1,29 +1,53 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql import functions as f
+from pyspark.sql.types import *
 
-spark = SparkSession.builder.appName("movie ratings counter").master("local").getOrCreate()
 
-input_path = "../input_data/u.data"
+class RatingCounter:
 
-movie_data = spark.read.csv(input_path, header=False, sep='\t')
+    def __init__(self):
+        self.spark = SparkSession.builder.appName("movie ratings counter").master("local").getOrCreate()
+        self.input_path = "../input_data/u.data"
+        self.output_path = '../output/ratings_count/'
 
-col_names = ['user_id', 'movie_id', 'rating', 'date_time']
+        self.col_names = ['user_id', 'movie_id', 'rating', 'date_time']
 
-movie_data = movie_data.toDF(*col_names)
-movie_data.createOrReplaceTempView('movie')
+    def get_schema(self):
+        return StructType([
+            StructField("user_id", IntegerType(), True),
+            StructField("movie_id", IntegerType(), True),
+            StructField("rating", IntegerType(), True),
+            StructField("date_time", StringType(), True)])
 
-rating_count = movie_data  \
-                .select(f.col('rating'))  \
-                .groupby(f.col('rating'))  \
-                .agg(f.count(f.col('rating')).alias('rating_count'))  \
-                .orderBy(f.col('rating'))
+    def data_read(self):
+        return self.spark.read.csv(self.input_path, schema=self.get_schema(), sep='\t')
 
-query = """select rating, count(1) as rating_count from movie group by rating order by rating"""
+    def data_read1(self):
+        data = self.spark.read.csv(self.input_path, inferSchema=True, sep='\t')
+        data = data.toDF(*self.col_names)
+        return data
 
-sql_rating_counter = spark.sql(query)
+    def ratings_counter(self, data_df:DataFrame):
+        return data_df.groupby('rating').agg(f.count('user_id').alias('ratings_count')).orderBy('ratings_count')
 
-rating_count.printSchema()
+    def ratings_counter_sql(self, movie_data_df):
+        movie_data_df.createOrReplaceTempView('movie')
+        query = """select rating, count(1) as rating_count from movie group by rating order by rating"""
+        return self.spark.sql(query)
 
-rating_count.show()
+    def data_writer(self, ratings_df):
+        ratings_df.write.parquet(self.output_path)
 
-sql_rating_counter.show()
+    def execute(self):
+        movie_data = self.data_read()
+
+        ratings = self.ratings_counter(movie_data)
+        ratings.show()
+
+        sql_ratings = self.ratings_counter_sql(movie_data)
+        sql_ratings.show()
+
+
+if __name__ == '__main__':
+    rc = RatingCounter()
+    rc.execute()
