c9c989fb963c48780d709049af85ca57eae3f15b
4256iq <bakul.seth@bp.com>
2025-02-06 07:12:11 +0000

    Auto-commit for sql 41 update on 2025-02-06 07:12:11

--- SQL/questions_and_solution_2/41_first_and_last_call.sql
+++ SQL/questions_and_solution_2/41_first_and_last_call.sql
@@ -1,6 +1,7 @@
 -- question statement
     -- Write an SQL to get the quite students in the class. Quite students are:
         -- Who tok at least one exam and did not score high score nor the low score in any on the exam.
+        -- Do not return the student who have not given any exam.
 
 -- create table statement
 create table phonelog(caller_id int, recipient_id int, date_called timestamp);
