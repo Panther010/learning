ada9fc9f96a7c042a9c9aaa110c62b29559ab2d2
Panther010 <48002647+Panther010@users.noreply.github.com>
2026-06-14 16:53:46 +0100

    SQl question updated

---
+++ SQL/questions_and_solution_3/54_Paypal_data_engineer.sql
@@ -0,0 +1,20 @@
+Write and sql code to find out table and below
+employ_id, employee_default_phone_number, totalentry, totallogin, totallogout, lastlogin, lastlogout
+
+employee_checkin_details:
+  employeeid ,entry_details, timestamp_details
+  1000 , login , 2023-06-16 01:00:15.34
+  1000 , login , 2023-06-16 02:00:15.34
+  1000 , login , 2023-06-16 03:00:15.34
+  1000 , logout , 2023-06-16 12:00:15.34
+  1001 , login , 2023-06-16 01:00:15.34
+  1001 , login , 2023-06-16 02:00:15.34
+  1001 , login , 2023-06-16 03:00:15.34
+  1001 , logout , 2023-06-16 12:00:15.34
+
+employee_details:
+employeeid , phone_number , isdefault
+1001 ,9999 , false
+1001 ,1111 , false
+1001 ,2222 , true
+1003 ,3333 , false
