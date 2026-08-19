0629239e9a5a7532271acdd1780fb6cbb43f397b
panther <bakulseth001@gmail.com>
2026-08-19 17:18:54 +0100

    Auto-commit for ai project to process linkedinpost on 2026-08-19 17:18:54

---
+++ projects/linkedin-post-generator/llm_helper.py
@@ -0,0 +1,15 @@
+from http.client import responses
+
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+import os
+
+
+load_dotenv()
+
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="openai/gpt-oss-20b")
+
+
+if __name__ == "__main__":
+    response = llm.invoke("do you have today top 2 news?")
+    print(response.content)
